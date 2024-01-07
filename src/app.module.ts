467d1967e4ad96b2acbb4e1143183cdd7779243b
Salahadin Dinsafa <salahadindinsafa2@gmail.com>
2024-01-07 15:43:48 +0300

    global config and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,42 @@
 import { Module } from '@nestjs/common/decorators/modules/module.decorator';
+import { APP_PIPE } from '@nestjs/core/constants';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi/lib/index';
 
 import { AuthModule } from './auth/auth.module';
 import { ormConfig } from './common/db/ormconfig.datasource';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USER: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            })
+        }),
         TypeOrmModule.forRootAsync({
             useFactory: ormConfig,
         }),
         AuthModule,
     ],
+    providers: [
+        {
+            provide: APP_PIPE,
+            useFactory: () => new ValidationPipe({
+                whitelist: true,
+                forbidNonWhitelisted: true,
+                transform: true,
+                transformOptions: { enableImplicitConversion: true }
+            })
+        }
+
+    ]
 })
 
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from "@nestjs/core";
+import {config} from 'dotenv';
 
 import { AppModule } from "./app.module"
+
+config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
